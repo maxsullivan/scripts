fec68ff77d45a02f52cbd36bec80dfade847841e
maxsullivan <maxsully@gmail.com>
2018-03-29 15:49:12 +1300

    Update tif_2_png.sh
    add comment about output directoy

--- tif_2_png.sh
+++ tif_2_png.sh
@@ -10,8 +10,7 @@ do
        dname=$(dirname "$file")
        echo "$fname"
        echo "$dname"
-       #echo ${file%.tif}
-       #echo convert -quality 70 -resize 50% -format png $file 
+#Output directory 'png_out' needs to be in place before running script.
        convert $file -quiet -quality 70 -resize 50% -format png $dname/png_out/$fname.png
 
 done
