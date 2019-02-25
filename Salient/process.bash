c8c8194f73bfdb45b7d951a7b175b6e1466bf161
maxsullivan <maxsully@gmail.com>
2019-02-26 08:57:00 +1300

    Created new bash file to process Salients

---
+++ Salient/process.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is a file to convert TEI to text
+XMLFILES="/home/sullivm1/RTD/nzetc/convert_files/Salient*199*"
+OUTPUTFILE="/home/sullivm1/RTD/nzetc/SalientResearch"
+
+for file in ${XMLFILES};
+do
+    echo processing $file
+    basename $file
+    echo $basename
+    base=`basename ${file}`
+    #echo $base
+    xsltproc --novalid ./tei_text.xsl $file > $OUTPUTFILE/$base.txt 
+
+done
